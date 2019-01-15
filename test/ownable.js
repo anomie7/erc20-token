472dbd2d493590041f77e6c218907a5c3aeb1dbf
Justin <anomie7777@gmail.com>
2019-01-15 17:59:21 +0900

    [impl] 유닛 테스트 작성 완료

---
+++ test/ownable.js
@@ -0,0 +1,22 @@
+const Owned = artifacts.require("Owned");
+
+contract("owner", async accounts => {
+  let owner = accounts[0];
+
+  it("should assert true", async () => {
+    let owned = await Owned.deployed();
+    assert.isTrue(await owned.isOwner());
+  });
+
+  it("should assert owner", async () => {
+    let owned = await Owned.deployed();
+    assert.equal(owner, await owned.owner());
+  });
+
+  it("should transfer Ownership", async () => {
+    let owned = await Owned.deployed();
+    let newOwner = accounts[1];
+    owned.transferOwnership(newOwner);
+    assert.equal(newOwner, await owned.owner());
+  });
+});
