16a494b0b1a92dc09783064f118d2725f3e5e0fa
Justin <anomie7777@gmail.com>
2019-01-16 16:18:34 +0900

    [add] migration 파일에 SafeMathMock 추가

--- migrations/2_deploy.contracts.js
+++ migrations/2_deploy.contracts.js
@@ -1,5 +1,6 @@
 var Owned = artifacts.require("./Owned.sol");
-
+var SafeMathMock = artifacts.require("./SafeMathMock.sol");
 module.exports = function(deployer) {
   deployer.deploy(Owned);
+  deployer.deploy(SafeMathMock);
 };
