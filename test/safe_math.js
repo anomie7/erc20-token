41e2c3de7bed946ef091152213bb44bee34af34c
Justin <anomie7777@gmail.com>
2019-01-16 16:18:55 +0900

    [test impl] safeMath의 테스트 작성

---
+++ test/safe_math.js
@@ -0,0 +1,33 @@
+const SafeMathMcok = artifacts.require("SafeMathMock");
+
+contract("SafeMathMcok", async accounts => {
+  it("should valid 3 plus 8", async () => {
+    let safeMath = await SafeMathMcok.deployed();
+    let result = await safeMath.add(3, 8);
+    assert.equal(11, result);
+  });
+
+  it("should invalid 3 plus -8", async () => {
+    let safeMath = await SafeMathMcok.deployed();
+    let result = await safeMath.add(3, -8);
+    assert.notEqual(-5, result);
+  });
+
+  it("is equal to 8 minus 3", async () => {
+    let safeMath = await SafeMathMcok.deployed();
+    let result = await safeMath.sub(8, 3);
+    assert.equal(5, result);
+  });
+
+  it("is not equal to 3 minus 8", async () => {
+    let safeMath = await SafeMathMcok.deployed();
+    try {
+      await safeMath.sub(3, 8);
+    } catch (error) {
+      assert.equal(
+        error.message,
+        "Returned error: VM Exception while processing transaction: revert"
+      );
+    }
+  });
+});
