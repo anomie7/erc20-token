cd4cc7aaa81dd2e8ab2e58e0b8aa14f033bd7161
Justin <anomie7777@gmail.com>
2019-01-17 19:32:58 +0900

    [modify] 테스트 코드 수정(before 라이프 사이클 훅을 이용)

--- test/ownable.js
+++ test/ownable.js
@@ -2,19 +2,21 @@ const Owned = artifacts.require("Owned");
 
 contract("owned", async accounts => {
   let owner = accounts[0];
+  let owned;
+
+  before(async function() {
+    owned = await Owned.deployed();
+  });
 
   it("should assert true", async () => {
-    let owned = await Owned.deployed();
     assert.isTrue(await owned.isOwner());
   });
 
   it("should assert owner", async () => {
-    let owned = await Owned.deployed();
     assert.equal(owner, await owned.owner());
   });
 
   it("should transfer Ownership", async () => {
-    let owned = await Owned.deployed();
     let newOwner = accounts[1];
     owned.transferOwnership(newOwner);
     assert.equal(newOwner, await owned.owner());
