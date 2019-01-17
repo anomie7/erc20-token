15e8a3d177655b72cba50d2cebd5b492c0e2ba76
Justin <anomie7777@gmail.com>
2019-01-17 19:33:30 +0900

    [modify] ERC20Mock 배포 스크립트 작성

--- migrations/2_deploy.contracts.js
+++ migrations/2_deploy.contracts.js
@@ -1,6 +1,9 @@
 var Owned = artifacts.require("./Owned.sol");
 var SafeMathMock = artifacts.require("./SafeMathMock.sol");
+var ERC20Mock = artifacts.require("./ERC20Mock.sol");
+
 module.exports = function(deployer) {
   deployer.deploy(Owned);
   deployer.deploy(SafeMathMock);
+  deployer.deploy(ERC20Mock);
 };
