8603def4a0f44592d1455a129911e67a6b20a8c4
Justin <anomie7777@gmail.com>
2019-01-30 16:26:12 +0900

    [impl] internal 메소드 구현

--- contracts/ownable/Owned.sol
+++ contracts/ownable/Owned.sol
@@ -3,5 +3,8 @@ pragma solidity ^0.5.0;
 import "./Ownable.sol";
 
 contract Owned is Ownable{
-  
+
+ function transferOwnership(address newOwner) public {
+     _transferOwnership(newOwner);
+    }
 }
